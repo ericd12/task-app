f26643e3a1ed9e725787b67521fb5aefbe9ae427
Taylor Radtke <taylorradtke@gmail.com>
2020-03-11 18:24:35 -0400

    missing ' in App.js

--- src/index.js
+++ src/index.js
@@ -1,124 +1,118 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@atlaskit/css-reset';
-import styled from 'styled-components';
-import { DragDropContext } from 'react-beautiful-dnd';
-import initialData from './initial-data';
-import Column from './column';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@atlaskit/css-reset";
+import styled from "styled-components";
+import { DragDropContext } from "react-beautiful-dnd";
+import axios from "axios";
+import initialData from "./initial-data";
+import Column from "./column";
 
 const Container = styled.div`
-    display: flex';
-
+  display: "flex";
 `;
 
-class App extends React.Component{
-    state = 
-            // elements: [],
-            initialData
-        
+class App extends React.Component {
+  state =
+    // elements: [],
+    initialData;
 
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/elements/")
+      .then(response => {
+        console.log({ response });
+        this.setState({ elements: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    componentDidMount(){
-        axios.get('http://localhost:5000/elements/')
-            .then(response => {
-                this.setState({elements: response.data})
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+  onDragEnd = result => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
+
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
     }
 
-
-
-    onDragEnd = result => {
-        document.body.style.color = 'inherit';
-        document.body.style.backgroundColor = 'inherit';
-
-        const {destination, source, draggableId } = result;
-
-        if(!destination){
-            return;
-        }
-
-        if(
-            destination.droppableId === source.droppableId &&
-            destination.index === source.index
-        ) {
-            return;
-        }
-
-        const start = this.state.columns[source.droppableId];
-        const finish = this.state.columns[destination.droppableId];
-
-        if(start === finish){
-            const newTaskIds = Array.from(start.taskIds);
-            newTaskIds.splice(source.index, 1);
-            newTaskIds.splice(destination.index, 0, draggableId);
-
-            const newColumn = {
-                ...start,
-                taskIds: newTaskIds
-            };
-
-            const newState = {
-                ...this.state,
-                columns: {
-                    ...this.state.columns,
-                    [newColumn.id]: newColumn
-                }
-            };
-
-            this.setState(newState);
-            return;
-        }
-
-        const startTaskIds = Array.from(start.taskIds);
-        startTaskIds.splice(source.index, 1);
-        const newStart = {
-            ...start,
-            taskIds: startTaskIds
-        };
-
-        const finishTaskIds = Array.from(finish.taskIds);
-        finishTaskIds.splice(destination.index, 0, draggableId);
-        const newFinish = {
-            ...finish,
-            taskIds: finishTaskIds
-        };
-
-        const newState = {
-            ...this.state,
-            columns: {
-                ...this.state.columns,
-                [newStart.id]: newStart,
-                [newFinish.id]: newFinish        
-            }
-        };
-
-        this.setState(newState);
-        return;
-    };        
-
-    render(){
-        // const col = this.state.initialData
-        return (
-        <DragDropContext onDragEnd = {this.onDragEnd}>
-            <Container>
-                {this.state.columnOrder.map(columnId =>{
-                    const column = this.state.columns[columnId];
-                    const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-
-                    return <Column key={column.id} column={column} tasks={tasks} />
-                })}
-            </Container>
-        </DragDropContext>
-        );
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
     }
+
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      this.setState(newState);
+      return;
+    }
+
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+
+    this.setState(newState);
+  };
+
+  render() {
+    // const col = this.state.initialData
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Container>
+          {this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(
+              taskId => this.state.tasks[taskId]
+            );
+
+            return <Column key={column.id} column={column} tasks={tasks} />;
+          })}
+        </Container>
+      </DragDropContext>
+    );
+  }
 }
 
-
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/task.js
+++ src/task.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
-import {Draggable} from 'react-beautiful-dnd';
+import React from "react";
+import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
-    border: 1px solid lightgrey;
-    border-radius: 2px;
-    padding: 8px;
-    margin-bottom: 8px;
-    background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
-
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragging ? "lightgreen" : "white")};
 `;
 
+const Task = ({ task, index }) => {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided, snapshot) => (
+        <Container
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
+        >
+          {task.content}
+        </Container>
+      )}
+    </Draggable>
+  );
+};
 
-
-export default class Task extends React.Component{
-    render(){
-        return (
-            <Draggable draggableId={this.props.task.id} index={this.props.index}>
-                {(provided, snapshot) => (
-                    <Container 
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                        isDragging={snapshot.isDragging}
-                    >
-                     
-                        {this.props.task.content}
-                    </Container>
-                )}
-            </Draggable> 
-        );
-    }
-}
+export default Task;
